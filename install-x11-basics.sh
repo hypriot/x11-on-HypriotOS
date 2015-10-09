c4fa75de445c47aa73a5c0f097842812a342bc6d
Stefan Scherer <scherer_stefan@icloud.com>
2015-10-09 11:01:38 +0200

    Install x11-server-utils to have xhost

--- install-x11-basics.sh
+++ install-x11-basics.sh
@@ -25,7 +25,7 @@ echo "...done"
 echo ""
 echo " STEP 2: install X11 and LightDM"
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbdev lxde lxde-common lightdm
+sudo apt-get install -y --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbdev lxde lxde-common lightdm x11-xserver-utils
 sudo apt-get install -y policykit-1 hal
 echo "...done"
 
